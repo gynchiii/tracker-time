01ffda5d6ed064549951b48c03bac699b1d5f576
gynchiii <thepatrinot@protonmail.com>
2022-06-04 03:26:57 -0300

    :rocket: 1st push

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -25,7 +25,7 @@ import { defineComponent } from "vue";
 import Temporizador from './Temporizador.vue'
 
 export default defineComponent({
-  name: "Formulário",
+  name: "Formulario",
   emits: ['aoSalvarTarefa'],
   components: {
     Temporizador
